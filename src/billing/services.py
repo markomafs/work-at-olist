1934f9f3f5317e447823cc5e2a7c26c53bda9a81
markomafs <marco.souza@tricae.com.br>
2018-07-05 18:52:53 -0300

    Add Billing Service
    to handle business logic between Model and Views or between Models

---
+++ src/billing/services.py
@@ -0,0 +1,14 @@
+from .models import Call, Billing, BillingRule
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class BillingService:
+    """
+    Service Responsible of handle business issues 
+    between models or models and views 
+    """
+
+    def create_billing(self, call=Call):
+        return True
