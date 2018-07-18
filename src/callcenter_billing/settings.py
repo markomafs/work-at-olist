ef81c39c46ba3785189c65a2c9511085260fbf81
markomafs <marco.souza@tricae.com.br>
2018-07-18 13:22:58 -0300

    Add Django_heroku

--- src/callcenter_billing/settings.py
+++ src/callcenter_billing/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import json_log_formatter
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -190,3 +192,5 @@ class CustomisedJSONFormatter(json_log_formatter.JSONFormatter):
     def json_record(self, message, extra, record):
         extra = super().json_record(message, extra, record)
         return extra
+
+django_heroku.settings(locals())
