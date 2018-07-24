ae849d7b61e1ce42070050474b4401d4d4b5fb8b
markomafs <marco.souza@tricae.com.br>
2018-07-23 23:51:53 -0300

    Update Bin Unittest Script

--- bin/unittest.sh
+++ bin/unittest.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_PATH="$DIR/../"
+cd ${ROOT_PATH}
+pipenv install --dev
 SRC_PATH="$DIR/../src"
 cd ${SRC_PATH}
-pytest --junitxml=test-results/junit/junit.xml --cov=billing/ --cov-report html:test-results/coverage/html --cov-report xml:test-results/coverage/coverage.xml
+
+export BILLING_LOG_LEVEL=DEBUG
+export LOG_FILE_PATH=/tmp/debug.log
+
+pipenv run pytest --junitxml=test-results/junit/junit.xml --cov=billing/ --cov-report html:test-results/coverage/html --cov-report xml:test-results/coverage/coverage.xml
